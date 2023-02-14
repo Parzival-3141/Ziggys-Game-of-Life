0c7c68bd562020d1082af2fcfd44f5cb37bb121b
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2023-02-14 15:41:16 -0500

    it worksgs

--- src/main.zig
+++ src/main.zig
@@ -5,15 +5,15 @@ const GRID_SIZE = 10;
 
 var grid = [GRID_SIZE * GRID_SIZE]u1{
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 1, 0,
+    0, 0, 0, 1, 0, 0, 0, 1, 1, 0,
+    0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+    0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
-    0, 0, 1, 0, 1, 0, 0, 0, 0, 0,
-    0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+    0, 0, 0, 1, 0, 0, 0, 1, 0, 0,
+    0, 0, 1, 0, 1, 0, 0, 1, 0, 0,
+    0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
 };
 
 var back_buffer = [1]u1{0} ** (GRID_SIZE * GRID_SIZE);
@@ -37,23 +37,27 @@ pub fn main() !void {
                 .{ 1, 0 },
                 .{ 1, 1 },
             }) |offset| {
-                const check_col = @mod(row + offset[0], GRID_SIZE);
-                const check_row = @mod(col + offset[1], GRID_SIZE);
+                const check_col = @mod(col + offset[0], GRID_SIZE);
+                const check_row = @mod(row + offset[1], GRID_SIZE);
                 const index = @intCast(u8, check_row * GRID_SIZE + check_col);
                 alive_count += grid[index];
             }
 
             const index = @intCast(u8, row * GRID_SIZE + col);
-            if (alive_count < 2 or alive_count > 3) {
-                back_buffer[index] = 0;
+            const is_alive = grid[index] == 1;
+
+            if (is_alive) {
+                back_buffer[index] = if (alive_count < 2 or alive_count > 3) 0 else 1;
             } else if (alive_count == 3) {
                 back_buffer[index] = 1;
+            } else {
+                back_buffer[index] = 0;
             }
-            printGrid(&back_buffer);
+            // printGrid(&back_buffer);
         }
     }
 
-    // printGrid(&back_buffer);
+    printGrid(&back_buffer);
 
     grid = back_buffer;
 }

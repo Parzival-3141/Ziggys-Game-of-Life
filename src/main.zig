f543c69cd9b7eea6d7c02ac167297a98f8048f7a
Stevie Hryciw <codroid@gmail.com>
2023-02-14 12:08:11 -0800

    Finish filling back buffer

--- src/main.zig
+++ src/main.zig
@@ -6,10 +6,10 @@ const GRID_SIZE = 10;
 var grid = [GRID_SIZE * GRID_SIZE]u1{
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+    0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 0, 1, 0, 0, 0,
-    0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -19,31 +19,51 @@ var grid = [GRID_SIZE * GRID_SIZE]u1{
 var back_buffer = [1]u1{0} ** (GRID_SIZE * GRID_SIZE);
 
 pub fn main() !void {
-    for (grid) |_, i| {
-        var count: u8 = 0;
+    printGrid(&grid);
 
-        for ([_]{
-            i - GRID_SIZE - 1, i - GRID_SIZE,     i - GRID_SIZE + 1,
-            i - 1,             i + 1,             i + GRID_SIZE - 1,
-            i + GRID_SIZE,     i + GRID_SIZE + 1,
-        }) |offset| {
-            count += grid[i + offset];
-        }
+    var row: i8 = 0;
+    while (row < GRID_SIZE) : (row += 1) {
+        var col: i8 = 0;
+        while (col < GRID_SIZE) : (col += 1) {
+            var alive_count: u8 = 0;
 
-        if (count < 2 or count > 3) {
-            back_buffer[i] = 0;
-        } else if (count == 3) {
-            back_buffer[i] = 1;
+            inline for (.{
+                .{ -1, -1 },
+                .{ -1, 0 },
+                .{ -1, 1 },
+                .{ 0, -1 },
+                .{ 0, 1 },
+                .{ 1, -1 },
+                .{ 1, 0 },
+                .{ 1, 1 },
+            }) |offset| {
+                const check_col = @mod(row + offset[0], GRID_SIZE);
+                const check_row = @mod(col + offset[1], GRID_SIZE);
+                const index = @intCast(u8, check_row * GRID_SIZE + check_col);
+                alive_count += grid[index];
+            }
+
+            const index = @intCast(u8, row * GRID_SIZE + col);
+            if (alive_count < 2 or alive_count > 3) {
+                back_buffer[index] = 0;
+            } else if (alive_count == 3) {
+                back_buffer[index] = 1;
+            }
+            printGrid(&back_buffer);
         }
     }
 
-    for (back_buffer) |value, i| {
+    // printGrid(&back_buffer);
+
+    grid = back_buffer;
+}
+
+fn printGrid(g: []u1) void {
+    for (g) |value, i| {
         if (i > 0 and i % GRID_SIZE == 0) {
             print("\n", .{});
         }
-
         print("{b}", .{value});
     }
-
-    grid = back_buffer;
+    print("\n----------\n", .{});
 }

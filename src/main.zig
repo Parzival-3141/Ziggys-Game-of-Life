025834d2520f826aa6700054746b11519b8ee735
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2023-03-01 20:06:12 -0500

    switch to SDL

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,10 @@
 const std = @import("std");
 const print = std.debug.print;
 
+const c = @cImport({
+    @cInclude("SDL2/SDL.h");
+});
+
 const GRID_SIZE = 10;
 
 var grid = [GRID_SIZE * GRID_SIZE]u1{
@@ -27,49 +31,97 @@ var grid = [GRID_SIZE * GRID_SIZE]u1{
 
 var back_buffer = [1]u1{0} ** (GRID_SIZE * GRID_SIZE);
 
+const window_width = 800;
+const window_height = window_width;
+
 pub fn main() !void {
-    var iter: usize = 0;
+    if (c.SDL_InitSubSystem(c.SDL_INIT_VIDEO) < 0) {
+        print("SDL failed to init!\n", .{});
+        return;
+    }
+    defer c.SDL_Quit();
 
-    while (iter < 100) : (iter += 1) {
-        printGrid(&grid);
+    const window = c.SDL_CreateWindow(
+        "ziggy's game of life",
+        c.SDL_WINDOWPOS_CENTERED,
+        c.SDL_WINDOWPOS_CENTERED,
+        window_width,
+        window_height,
+        0,
+    );
 
-        var row: i8 = 0;
-        while (row < GRID_SIZE) : (row += 1) {
-            var col: i8 = 0;
-            while (col < GRID_SIZE) : (col += 1) {
-                var alive_count: u8 = 0;
+    const renderer = c.SDL_CreateRenderer(window, -1, c.SDL_RENDERER_ACCELERATED);
 
-                inline for (.{
-                    .{ -1, -1 },
-                    .{ -1, 0 },
-                    .{ -1, 1 },
-                    .{ 0, -1 },
-                    .{ 0, 1 },
-                    .{ 1, -1 },
-                    .{ 1, 0 },
-                    .{ 1, 1 },
-                }) |offset| {
-                    const check_col = @mod(col + offset[0], GRID_SIZE);
-                    const check_row = @mod(row + offset[1], GRID_SIZE);
-                    const index = @intCast(u8, check_row * GRID_SIZE + check_col);
-                    alive_count += grid[index];
-                }
+    var event: c.SDL_Event = undefined;
+    var quitting = false;
+    var paused = true;
 
-                const index = @intCast(u8, row * GRID_SIZE + col);
-                const is_alive = grid[index] == 1;
+    while (!quitting) {
+        while (c.SDL_PollEvent(&event) != 0) {
+            switch (event.type) {
+                c.SDL_KEYDOWN => {
+                    print("{s}\n", .{c.SDL_GetKeyName(event.key.keysym.sym)});
+                    switch (event.key.keysym.sym) {
+                        c.SDLK_SPACE => paused = !paused,
+                        else => {},
+                    }
+                },
 
-                if (is_alive) {
-                    back_buffer[index] = if (alive_count < 2 or alive_count > 3) 0 else 1;
-                } else if (alive_count == 3) {
-                    back_buffer[index] = 1;
-                } else {
-                    back_buffer[index] = 0;
-                }
+                c.SDL_QUIT => {
+                    quitting = true;
+                },
+
+                else => {},
             }
         }
 
-        grid = back_buffer;
-        std.time.sleep(100 * std.time.ns_per_ms);
+        // printGrid(&grid);
+        _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        _ = c.SDL_RenderClear(renderer);
+
+        if (!paused) {
+            _ = c.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            _ = c.SDL_RenderDrawRect(renderer, &[_]c.SDL_Rect{.{ .x = window_width / 2, .y = window_height / 2, .w = 10, .h = 10 }});
+
+            var row: i8 = 0;
+            while (row < GRID_SIZE) : (row += 1) {
+                var col: i8 = 0;
+                while (col < GRID_SIZE) : (col += 1) {
+                    var alive_count: u8 = 0;
+
+                    inline for (.{
+                        .{ -1, -1 },
+                        .{ -1, 0 },
+                        .{ -1, 1 },
+                        .{ 0, -1 },
+                        .{ 0, 1 },
+                        .{ 1, -1 },
+                        .{ 1, 0 },
+                        .{ 1, 1 },
+                    }) |offset| {
+                        const check_col = @mod(col + offset[0], GRID_SIZE);
+                        const check_row = @mod(row + offset[1], GRID_SIZE);
+                        const index = @intCast(u8, check_row * GRID_SIZE + check_col);
+                        alive_count += grid[index];
+                    }
+
+                    const index = @intCast(u8, row * GRID_SIZE + col);
+                    const is_alive = grid[index] == 1;
+
+                    if (is_alive) {
+                        back_buffer[index] = if (alive_count < 2 or alive_count > 3) 0 else 1;
+                    } else if (alive_count == 3) {
+                        back_buffer[index] = 1;
+                    } else {
+                        back_buffer[index] = 0;
+                    }
+                }
+            }
+            grid = back_buffer;
+        }
+
+        c.SDL_RenderPresent(renderer);
+        // c.SDL_Delay(1);
     }
 }
 

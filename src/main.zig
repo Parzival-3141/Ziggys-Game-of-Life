64b06370150f580d1a56d68f536f0badfb4d883e
Stevie Hryciw <codroid@gmail.com>
2023-03-01 20:03:50 -0800

    Mouse-based cursor, decouple FPS from cell update rate

--- src/main.zig
+++ src/main.zig
@@ -39,7 +39,7 @@ const window_width = 800;
 const window_height = window_width;
 const cell_width = window_width / GRID_SIZE;
 const cell_height = window_height / GRID_SIZE;
-const ticks_per_second = 10;
+const updates_per_second = 10;
 
 pub fn main() !void {
     if (c.SDL_InitSubSystem(c.SDL_INIT_VIDEO) < 0) {
@@ -62,21 +62,24 @@ pub fn main() !void {
     var event: c.SDL_Event = undefined;
     var quitting = false;
     var paused = true;
+    var cell_tick_timer: u32 = 0;
+    var previous_tick = c.SDL_GetTicks();
 
     while (!quitting) {
+        var clicked = false;
+
         while (c.SDL_PollEvent(&event) != 0) {
             switch (event.type) {
+                c.SDL_MOUSEBUTTONDOWN => {
+                    if (event.button.button == c.SDL_BUTTON_LEFT) clicked = true;
+                },
                 c.SDL_KEYDOWN => {
                     switch (event.key.keysym.sym) {
                         c.SDLK_SPACE => paused = !paused,
                         else => {},
                     }
                 },
-
-                c.SDL_QUIT => {
-                    quitting = true;
-                },
-
+                c.SDL_QUIT => quitting = true,
                 else => {},
             }
         }
@@ -84,83 +87,95 @@ pub fn main() !void {
         // printGrid(&grid);
 
         if (!paused) {
-            var row: i8 = 0;
-            while (row < GRID_SIZE) : (row += 1) {
-                var col: i8 = 0;
-                while (col < GRID_SIZE) : (col += 1) {
-                    var alive_count: u8 = 0;
-
-                    inline for (.{
-                        .{ -1, -1 },
-                        .{ -1, 0 },
-                        .{ -1, 1 },
-                        .{ 0, -1 },
-                        .{ 0, 1 },
-                        .{ 1, -1 },
-                        .{ 1, 0 },
-                        .{ 1, 1 },
-                    }) |offset| {
-                        const check_col = @mod(col + offset[0], GRID_SIZE);
-                        const check_row = @mod(row + offset[1], GRID_SIZE);
-                        const index = @intCast(u8, check_row * GRID_SIZE + check_col);
-                        alive_count += grid[index];
-                    }
-
-                    const index = @intCast(u8, row * GRID_SIZE + col);
-                    const is_alive = grid[index] == 1;
-
-                    if (is_alive) {
-                        back_buffer[index] = if (alive_count < 2 or alive_count > 3) 0 else 1;
-                    } else if (alive_count == 3) {
-                        back_buffer[index] = 1;
-                    } else {
-                        back_buffer[index] = 0;
-                    }
-                }
+            const now = c.SDL_GetTicks();
+            const dt = now - previous_tick;
+            cell_tick_timer += dt;
+            if (cell_tick_timer >= 1000 / updates_per_second) {
+                cell_tick_timer = 0;
+                updateGrid();
             }
-            grid = back_buffer;
+            previous_tick = now;
         }
 
         // Draw automata state
-        _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        _ = c.SDL_RenderClear(renderer);
-        _ = c.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        var row: u32 = 0;
-        while (row < GRID_SIZE) : (row += 1) {
-            var col: u32 = 0;
-            while (col < GRID_SIZE) : (col += 1) {
-                const alive = grid[row * GRID_SIZE + col] == 1;
-                if (!alive) continue;
-                const x = @intCast(c_int, col * cell_width);
-                const y = @intCast(c_int, row * cell_height);
-                _ = c.SDL_RenderFillRect(renderer, &[_]c.SDL_Rect{.{
-                    .x = x,
-                    .y = y,
-                    .w = cell_width,
-                    .h = cell_height,
-                }});
+        {
+            _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            _ = c.SDL_RenderClear(renderer);
+            _ = c.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            var row: u32 = 0;
+            while (row < GRID_SIZE) : (row += 1) {
+                var col: u32 = 0;
+                while (col < GRID_SIZE) : (col += 1) {
+                    const alive = grid[row * GRID_SIZE + col] == 1;
+                    if (!alive) continue;
+                    const x = @intCast(c_int, col * cell_width);
+                    const y = @intCast(c_int, row * cell_height);
+                    _ = c.SDL_RenderFillRect(renderer, &[_]c.SDL_Rect{.{
+                        .x = x,
+                        .y = y,
+                        .w = cell_width,
+                        .h = cell_height,
+                    }});
+                }
             }
         }
 
+        // Draw selection
+        {
+            var x: c_int = undefined;
+            var y: c_int = undefined;
+            _ = c.SDL_GetMouseState(&x, &y);
+            const norm_x = @intToFloat(f32, x) / @intToFloat(f32, window_width);
+            const norm_y = @intToFloat(f32, y) / @intToFloat(f32, window_height);
+            const col = @floatToInt(u32, @floor(norm_x * GRID_SIZE));
+            const row = @floatToInt(u32, @floor(norm_y * GRID_SIZE));
+            _ = c.SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+            _ = c.SDL_RenderDrawRect(renderer, &[_]c.SDL_Rect{.{
+                .x = @intCast(c_int, col * cell_width),
+                .y = @intCast(c_int, row * cell_height),
+                .w = @intCast(c_int, cell_width),
+                .h = @intCast(c_int, cell_height),
+            }});
+        }
+
         c.SDL_RenderPresent(renderer);
-        c.SDL_Delay(1000.0 / ticks_per_second);
     }
 }
 
-fn printGrid(g: []const u1) void {
-    var stdout_hndl = std.io.getStdOut();
-    stdout_hndl.writeAll(&.{ 0o33, '[', '2', 'J' }) catch unreachable;
+fn updateGrid() void {
+    var row: i8 = 0;
+    while (row < GRID_SIZE) : (row += 1) {
+        var col: i8 = 0;
+        while (col < GRID_SIZE) : (col += 1) {
+            var alive_count: u8 = 0;
 
-    print("----------\n", .{});
-    for (g, 0..) |value, i| {
-        if (i > 0 and i % GRID_SIZE == 0) {
-            print("\n", .{});
+            inline for (.{
+                .{ -1, -1 },
+                .{ -1, 0 },
+                .{ -1, 1 },
+                .{ 0, -1 },
+                .{ 0, 1 },
+                .{ 1, -1 },
+                .{ 1, 0 },
+                .{ 1, 1 },
+            }) |offset| {
+                const check_col = @mod(col + offset[0], GRID_SIZE);
+                const check_row = @mod(row + offset[1], GRID_SIZE);
+                const index = @intCast(u8, check_row * GRID_SIZE + check_col);
+                alive_count += grid[index];
+            }
+
+            const index = @intCast(u8, row * GRID_SIZE + col);
+            const is_alive = grid[index] == 1;
+
+            if (is_alive) {
+                back_buffer[index] = if (alive_count < 2 or alive_count > 3) 0 else 1;
+            } else if (alive_count == 3) {
+                back_buffer[index] = 1;
+            } else {
+                back_buffer[index] = 0;
+            }
         }
-        const char: u8 = switch (value) {
-            0 => ' ',
-            1 => '#',
-        };
-        print("{c}", .{char});
     }
-    print("\n----------\n", .{});
+    grid = back_buffer;
 }
